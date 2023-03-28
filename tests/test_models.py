8313ae227086c10daea5a4beee4f8aa9b9155eeb
Anja Le Blanc <anja.leblanc@manchester.ac.uk>
2023-03-28 15:48:22 +0100

    add parameterized mean test

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,41 @@
 import pandas as pd
 import pandas.testing as pdt
 import datetime
+import pytest
 
+import pytest
+
+
+@pytest.mark.parametrize(
+    "test_data, test_index, test_columns, expected_data, expected_index, expected_columns",
+    [
+        (
+            [[0.0, 0.0],[0.0,0.0],[0.0,0.0]],
+            [ pd.to_datetime('2000-01-01 01:00'),
+              pd.to_datetime('2000-01-01 01:00'),
+              pd.to_datetime('2000-01-01 01:00') ],
+            ['A', 'B'],
+            [[0.0, 0.0]],
+            [datetime.date(2000,1,1)],
+            ['A', 'B']
+        ),
+        (
+            [[1, 2],[3, 4],[5,6]],
+            [ pd.to_datetime('2000-01-01 01:00'),
+              pd.to_datetime('2000-01-01 01:00'),
+              pd.to_datetime('2000-01-01 01:00') ],
+            ['A', 'B'],
+            [[3.0, 4.0]],
+            [datetime.date(2000,1,1)],
+            ['A', 'B']
+        )
+    ]
+)
+def test_daily_mean(test_data, test_index, test_columns, expected_data, expected_index, expected_columns):
+    """Tst mean function works with zeros and positive integer"""
+    from catchment.models import daily_mean
+    pdt.assert_frame_equal(daily_mean(pd.DataFrame(data=test_data, index=test_index, columns=test_columns)),
+                           pd.DataFrame(data=expected_data, index=expected_index, columns=expected_columns))
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
